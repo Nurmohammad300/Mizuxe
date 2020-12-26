3555f48ccddd59fd536d93fba152529a72266ac7
Nur Mohammad <73306001+Nurmohammad300@users.noreply.github.com>
2020-12-26 13:33:29 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     $('a').on('click',function(event){
+//         if (this.event !==""){
+
+//             event.preventDefault();
+
+//             let hash = this.hash;
+//             $('html,body').animate({
+//                 scrollTop: $(hash).offset().top},800,function(){
+//                     window.location.hash= hash;
+//             });
+//         };
+//     });
+// });
