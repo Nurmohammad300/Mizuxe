e89ecd789fbc298ecc18fe7fdf5b11ae901da703
Nur Mohammad <73306001+Nurmohammad300@users.noreply.github.com>
2020-12-26 13:37:32 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-// $(document).ready(function(){
-//     $('a').on('click',function(event){
-//         if (this.event !==""){
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
 
-//             event.preventDefault();
-
-//             let hash = this.hash;
-//             $('html,body').animate({
-//                 scrollTop: $(hash).offset().top},800,function(){
-//                     window.location.hash= hash;
-//             });
-//         };
-//     });
-// });
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
